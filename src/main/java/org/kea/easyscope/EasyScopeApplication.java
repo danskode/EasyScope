b252512012810f1bc1eb8e82ee8357be1009cbe8
danskode <149320369+danskode@users.noreply.github.com>
2024-11-22 15:54:16 +0100

    Initial commit

---
+++ src/main/java/org/kea/easyscope/EasyScopeApplication.java
@@ -0,0 +1,13 @@
+package org.kea.easyscope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyScopeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyScopeApplication.class, args);
+    }
+
+}
