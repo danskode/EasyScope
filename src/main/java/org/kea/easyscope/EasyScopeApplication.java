3cab816d8853d9e32196bb5f8c5e652860176b64
danskode <149320369+danskode@users.noreply.github.com>
2024-11-25 12:48:49 +0100

    3x profiler commit

--- src/main/java/org/kea/easyscope/EasyScopeApplication.java
+++ src/main/java/org/kea/easyscope/EasyScopeApplication.java
@@ -1,10 +1,15 @@
 package org.kea.easyscope;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EasyScopeApplication {
+
+    // Logger til at se resultater i terminalen ...
+    // private Logger logger = LoggerFactory.getLogger(EasyScopeApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(EasyScopeApplication.class, args);
