cb2e97b0f61e8f3deee85fa26ccdae0ae13829ad
danskode <149320369+danskode@users.noreply.github.com>
2024-12-12 10:18:33 +0100

    Added a basic u test to see if it needs tests ...

---
+++ src/test/java/org/kea/easyscope/BasicTest.java
@@ -0,0 +1,10 @@
+package org.kea.easyscope;
+
+public class BasicTest {
+
+    // A simple test that will always pass
+    @Test
+    public void simpleTest() {
+        assertTrue(true, "This test will always pass");
+    }
+}
